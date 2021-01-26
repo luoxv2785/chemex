3d731d57392ccbcc35f2e06e97b82fe559042457
celaraze <celaraze@qq.com>
2021-01-27 00:08:11 +0800

    增强：增加selectCreate字段类型

--- dcat-admin-extensions/celaraze/dcat-extension-setting/src/Http/routes.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Celaraze\DcatSetting\Http\Controllers\DcatSettingController;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/settings/site', [DcatSettingController::class, 'index'])
-    ->name('settings.site.index');
