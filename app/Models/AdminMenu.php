d06bbd97b8c60fafdb9c2e7650f4a8debe7208e9
celaraze <celaraze@qq.com>
2021-02-09 20:01:52 +0800

    优化：演示模式修复

--- app/Models/AdminMenu.php
+++ app/Models/AdminMenu.php
@@ -10,4 +10,14 @@ class AdminMenu extends Menu
 {
     use HasFactory;
     use HasDateTimeFormatter;
+
+    protected static function booted()
+    {
+        // 保存回调，demo模式下不允许修改管理员信息
+        static::saving(function () {
+            if (config('admin.demo')) {
+                abort(401, '演示模式下不允许修改');
+            }
+        });
+    }
 }
