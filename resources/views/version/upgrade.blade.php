807e659dc877306f8affb6483f09554e33ea28d1
Celaraze <celaraze@qq.com>
2021-02-18 11:55:11 +0800

    快照：增加更新

--- resources/views/version/upgrade.blade.php
+++ resources/views/version/upgrade.blade.php
@@ -5,14 +5,15 @@
      * 升级
      */
     function upgrade() {
-        document.getElementById('upgrade').disabled = true;
+        document.getElementById('upgrade').attr('disabled', "true");
+        document.getElementById('upgrade').innerText = '正在更新';
         $.ajax({
             url: "/version/upgrade",
             success: function (res) {
-                if (res.original.code === 200) {
-                    Dcat.success(res.original.message);
+                if (res.data.code === 200) {
+                    Dcat.success(res.data.message);
                 } else {
-                    Dcat.warning(res.original.message);
+                    Dcat.warning(res.data.message);
                 }
             },
             error: function (res) {
