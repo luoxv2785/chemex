e390ff255d6fa9f09409ef5538b24f48227191c0
Celaraze <celaraze@qq.com>
2021-03-02 14:53:59 +0800

    update: composer dump-autoload

--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -174,7 +174,7 @@ final class Versions
   'sebastian/type' => '2.3.1@81cd61ab7bbf2de744aba0ea61fae32f721df3d2',
   'sebastian/version' => '3.0.2@c6c1022351a901512170118436c764e473f6de8c',
   'theseer/tokenizer' => '1.2.0@75a63c33a8577608444246075ea0af0d052e452a',
-  'celaraze/chemex' => 'dev-main@42f0b5841b4a3c003bc5dd4b96f92e31f67bf10e',
+  'celaraze/chemex' => 'dev-main@77c422f8bb1ac4d0a8b6ae7b63c08c2a24ffb4a8',
 );
 
     private function __construct()
