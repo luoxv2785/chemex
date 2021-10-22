6eb6774cb7a333a87c525a0bd2ab0d2ea0ff7bbb
纯小白 <758484820@qq.com>
2021-10-22 00:41:54 +0000

    增加清理缓存核心文件 增加清理缓存核心文件

---
+++ app/Admin/Actions/form/Clear.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Admin\Actions\Form;
+
+use Dcat\Admin\Actions\Response;
+use Dcat\Admin\Form\AbstractTool;
+use Dcat\Admin\Traits\HasPermissions;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
+use PHPUnit\Runner\Exception;
+
+class Clear extends AbstractTool
+{
+    /**
+     * @return string
+     */
+	protected $title = '点击继续';
+
+    /**
+     * Handle the action request.
+     *
+     * @param Request $request
+     *
+     * @return Response
+     */
+    public function handle(Request $request)
+    {
+
+        try {
+            Artisan::call('cache:clear');
+            return $this->response()
+                ->success('清理成功')
+                ->redirect('/');
+        } catch (Exception $e) {
+            return $this->response()
+                ->success('清理失败')
+                ->redirect('/');
+        }
+    }
+
+    /**
+     * @return string|void
+     */
+    protected function href()
+    {
+        // return admin_url('auth/users');
+    }
+
+    /**
+	 * @return string|array|void
+	 */
+	public function confirm()
+	{
+		 return ['是否彻底清理缓存?', '请确认'];
+	}
+
+    /**
+     * @param Model|Authenticatable|HasPermissions|null $user
+     *
+     * @return bool
+     */
+    protected function authorize($user): bool
+    {
+        return true;
+    }
+
+    /**
+     * @return array
+     */
+    protected function parameters()
+    {
+        return [];
+    }
+}
