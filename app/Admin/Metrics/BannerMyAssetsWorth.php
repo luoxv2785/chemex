6df6b324a1b5b46f151bfce327cd84787ab53996
Yeahlo <yeahlo@gmail.com>
2021-12-23 17:42:41 +0800

    【轻量级 PR】：修复使用ADMIN_ROUTE_PREFIX时仪表盘部分背景显示错误

--- app/Admin/Metrics/BannerMyAssetsWorth.php
+++ app/Admin/Metrics/BannerMyAssetsWorth.php
@@ -26,9 +26,10 @@ class BannerMyAssetsWorth extends Card
         $value = DeviceRecord::count();
         $text = trans('main.device_counts');
         $route = admin_route('device.records.index');
+        $bg = url('static/images/green.png');
         $html = <<<HTML
 <a href="{$route}">
-<div class="small-box" style="padding:0 20px;height:100px;margin-bottom: 0;border-radius: .25rem;background: url('static/images/green.png') no-repeat;background-size: 100% 100%;">
+<div class="small-box" style="padding:0 20px;height:100px;margin-bottom: 0;border-radius: .25rem;background: url('{$bg}') no-repeat;background-size: 100% 100%;">
     <div class="inner">
         <h4 style="color: white;font-size: 30px;text-shadow: #888888 1px 1px 2px;">{$value}</h4>
         <p style="color: white;text-shadow: #888888 1px 1px 2px;">{$text}</p>
