2c1cc53bb07d978ba919efb93623aab463e658f7
celaraze <celaraze@qq.com>
2022-03-02 16:46:03 +0800

    修复jwt遗留组件的一些问题

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -5,14 +5,14 @@ set -e
 source /var/www/chemex/.env
 
 # 等待数据库服务启动
-while ! mysqladmin ping -h"$DB_HOST" -u"$DB_USERNAME" -p"$DB_PASSWORD" -P"${DB_PORT:-3306}" --silent; do
-  echo "数据库服务还未响应，继续等待"
-  sleep 3
+while ! mysqladmin ping -h"$APP_DATABASE_HOST" -u"$APP_DATABASE_USERNAME" -p"$APP_DATABASE_PASSWORD" -P"${APP_DATABASE_PORT:-3306}" --silent; do
+    echo "数据库服务还未响应，继续等待"
+    sleep 3
 done
 
 # 初始化应用程序
 # [ -z "${APP_KEY}" ]
-php artisan jwt:secret -f && php artisan chemex:install
+php artisan chemex:install
 
 # 启动应用程序
 apache2-foreground
